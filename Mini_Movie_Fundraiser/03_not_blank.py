02d364fe7ee414fc03015e53df6f5b7f3384c7d1
narayanj1193 <101072172+narayanj1193@users.noreply.github.com>
2024-02-07 12:26:39 +1300

    Add files via upload

---
+++ Mini_Movie_Fundraiser/03_not_blank.py
@@ -0,0 +1,24 @@
+# function goes at top
+
+# function checks that the users input is not blank
+def not_blank(question):
+
+    while True:
+        response = input(question).strip
+
+        # if the response is blank, outputs error
+        if response == "":
+            print("Sorry, this can't be blank or contain only spaces. Please try again")
+        else:
+            return response
+
+
+# main routine goes here
+
+while True:
+    name = not_blank("What is your name? ")
+
+    if name == "xxx":
+        break
+
+    print(f"You entered {name}")
